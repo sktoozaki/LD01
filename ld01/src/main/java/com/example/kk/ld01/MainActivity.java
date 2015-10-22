70ec7a5f1589a171a63136512c912aadb5e34f94
sktoozaki <skyjeankk@163.com>
2015-10-22 17:31:54 +0800

    Loginpage added

---
+++ ld01/src/main/java/com/example/kk/ld01/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.kk.ld01;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+
+    }
+}
