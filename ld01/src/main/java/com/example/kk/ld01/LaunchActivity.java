4361e495c97a921b1ac1edd6b3dbdd0771a57e44
sktoozaki <skyjeankk@163.com>
2015-11-18 11:03:51 +0800

    TitleActivity Layout added

--- ld01/src/main/java/com/example/kk/ld01/LaunchActivity.java
+++ ld01/src/main/java/com/example/kk/ld01/LaunchActivity.java
@@ -2,10 +2,13 @@ package com.example.kk.ld01;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class LaunchActivity extends Activity {
     private ImageView mLogo,mDot1,mDot2,mDot3;
+    private LinearLayout mLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,8 +16,14 @@ public class LaunchActivity extends Activity {
         initViews();
     }
 
-    private void initViews() {
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
 
+
+        return super.onTouchEvent(event);
+    }
+
+    private void initViews() {
 
     }
 }
