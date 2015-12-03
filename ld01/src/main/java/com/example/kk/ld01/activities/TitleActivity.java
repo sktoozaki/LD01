b0e0d70ddea2994d671d4db9f8d6db613a61e3b7
GUO Jian <skyjeankk@163.com>
2015-12-03 13:57:37 +0800

    Modify SignUpActivity

--- ld01/src/main/java/com/example/kk/ld01/activities/TitleActivity.java
+++ ld01/src/main/java/com/example/kk/ld01/activities/TitleActivity.java
@@ -3,23 +3,14 @@ package com.example.kk.ld01.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.transition.AutoTransition;
-import android.transition.ChangeBounds;
-import android.transition.Scene;
-import android.transition.Transition;
-import android.transition.TransitionInflater;
-import android.transition.TransitionManager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 
 import com.example.kk.ld01.R;
 
 public class TitleActivity extends Activity {
 
-    private Button btnSkip,btnSignin;
-    private Scene mAScene,mBScene;
-    private FrameLayout mSceneRoot;
+    private Button btnSignup,btnSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +20,7 @@ public class TitleActivity extends Activity {
     }
 
     private void initViews() {
-        mSceneRoot= (FrameLayout) findViewById(R.id.scene_root);
-        mAScene=Scene.getSceneForLayout(mSceneRoot,R.layout.a_scene,this);
-        mBScene=Scene.getSceneForLayout(mSceneRoot,R.layout.b_scene,this);
-
-
-        btnSkip= (Button) findViewById(R.id.skip_titleA_btn);
+        btnSignup = (Button) findViewById(R.id.signup_titleA_btn);
         btnSignin= (Button) findViewById(R.id.signin_titleA_btn);
         btnSignin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,13 +29,10 @@ public class TitleActivity extends Activity {
             }
         });
 
-        btnSkip.setOnClickListener(new View.OnClickListener() {
+        btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TransitionInflater inflater=TransitionInflater.from(TitleActivity.this);
-                Transition mChangeBounds=inflater.inflateTransition(R.transition.changebounds_trans);
-                TransitionManager.go(mBScene,mChangeBounds);
-
+                startActivity(new Intent(TitleActivity.this,SignUpActivity.class));
             }
         });
     }
