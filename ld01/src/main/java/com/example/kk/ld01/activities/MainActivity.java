df7d803e2a87af383c84e429dd76d9b7a3440b4b
sktoozaki <skyjeankk@163.com>
2016-02-01 17:16:37 +0800

    Update DayFragment

--- ld01/src/main/java/com/example/kk/ld01/activities/MainActivity.java
+++ ld01/src/main/java/com/example/kk/ld01/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kk.ld01.activities;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
@@ -13,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -40,6 +42,7 @@ import java.util.List;
 @ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
 
+    private DayFragment mDayFragment;
     private AVUser mUser;
     private List<TaskItem> taskList;
     private CommonAdapter listAdapter;
@@ -51,7 +54,7 @@ public class MainActivity extends BaseActivity {
     };
 
     @ViewInject(R.id.framelayout_main)
-    private FrameLayout mFrame;
+    private LinearLayout mFrame;
 
     @ViewInject(R.id.toolbar_main)
     private Toolbar mToolbar;
@@ -110,14 +113,42 @@ public class MainActivity extends BaseActivity {
         }
     }
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initViews();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initService();
+    }
+
+    //禁用返回
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        // 连按两次返回键退出程序
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if ((System.currentTimeMillis() - exitTime) < 2000) {
+                finish();
+                System.exit(0);
+            }
+            exitTime = System.currentTimeMillis();
+            Toast.makeText(this, "再按一次退出程序！", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /*初始化UI*/
     private void initViews() {
         dateTimeNow=new DateTime();
         dateTimeThisWeek= getDateTimeThisWeek(dateTimeNow);
@@ -142,53 +173,38 @@ public class MainActivity extends BaseActivity {
         mToolbar.setSubtitleTextColor(0xffffffff);
         setSupportActionBar(mToolbar);
 
-        /*mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.action_search:
-                        Toast.makeText(MainActivity.this, "action_search", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.action_filter:
-                        Toast.makeText(MainActivity.this, "action_filter", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.action_settings:
-                        Toast.makeText(MainActivity.this, "action_settings", Toast.LENGTH_SHORT).show();
-                        mUser.logOut();
-                        finish();
-                        System.exit(0);
-                        break;
-                }
-                return true;
-            }
-        });*/
-
         mWeekRG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 switch (checkedId) {
                     case R.id.mon_rb_main:
                         Log.d("test", "mon");
-                        loadFragment(days.MONDAY);
-                        getTasks(mUser.getUsername(),dateTimeThisWeek.get(0));
+                        getTasks(mUser.getUsername(), dateTimeThisWeek.get(0));
+                        loadFragment(dateTimeThisWeek.get(0));
                         break;
                     case R.id.tue_rb_main:
-                        getTasks(mUser.getUsername(),dateTimeThisWeek.get(1));
+                        getTasks(mUser.getUsername(), dateTimeThisWeek.get(1));
+                        loadFragment(dateTimeThisWeek.get(1));
                         break;
                     case R.id.wed_rb_main:
-                        getTasks(mUser.getUsername(),dateTimeThisWeek.get(2));
+                        getTasks(mUser.getUsername(), dateTimeThisWeek.get(2));
+                        loadFragment(dateTimeThisWeek.get(2));
                         break;
                     case R.id.thu_rb_main:
-                        getTasks(mUser.getUsername(),dateTimeThisWeek.get(3));
+                        getTasks(mUser.getUsername(), dateTimeThisWeek.get(3));
+                        loadFragment(dateTimeThisWeek.get(3));
                         break;
                     case R.id.fri_rb_main:
-                        getTasks(mUser.getUsername(),dateTimeThisWeek.get(4));
+                        getTasks(mUser.getUsername(), dateTimeThisWeek.get(4));
+                        loadFragment(dateTimeThisWeek.get(4));
                         break;
                     case R.id.sat_rb_main:
-                        getTasks(mUser.getUsername(),dateTimeThisWeek.get(5));
+                        getTasks(mUser.getUsername(), dateTimeThisWeek.get(5));
+                        loadFragment(dateTimeThisWeek.get(5));
                         break;
                     case R.id.sun_rb_main:
-                        getTasks(mUser.getUsername(),dateTimeThisWeek.get(6));
+                        getTasks(mUser.getUsername(), dateTimeThisWeek.get(6));
+                        loadFragment(dateTimeThisWeek.get(6));
                         break;
                     default:
                         break;
@@ -196,7 +212,6 @@ public class MainActivity extends BaseActivity {
             }
         });
 
-        setDefaultFragment();
 
         //TODO 功能化ToolBar
 
@@ -207,22 +222,28 @@ public class MainActivity extends BaseActivity {
         //TODO 添加CalendarView与下拉事件
 
         //TODO 获取服务中的Task
-        initService();
+
+        loadFragment(dateTimeNow);
     }
 
-    private void setDefaultFragment() {
-        FragmentManager fragment=getFragmentManager();
-        FragmentTransaction transaction=fragment.beginTransaction();
-        DayFragment mDayFragment=new DayFragment(dateTimeNow);
+    /*初始化服务请求*/
+    private void initService() {
+        mUser=AVUser.getCurrentUser();
+        if (mUser!=null){
+            getTasks(mUser.getUsername(),dateTimeNow);
+        }else {
+        }
     }
 
-    private void loadFragment(days day) {
-
-
+    /*加载特定DateTime的Fragment*/
+    private void loadFragment(DateTime datetime) {
+        DayFragment mDayFragment=new DayFragment(datetime);
+        FragmentTransaction transaction=getFragmentManager().beginTransaction();
+        transaction.replace(R.id.framelayout_main,mDayFragment);
+        transaction.commit();
     }
 
-
-    //获取本周每一天的DateTime
+    /*获取本周每一天的DateTime*/
     private List<DateTime> getDateTimeThisWeek(DateTime dateTimeNow) {
         DateTime thisMonday=dateTimeNow.minusDays(dateTimeNow.getDayOfWeek()-1);
         List<DateTime> list=new ArrayList<>();
@@ -233,6 +254,7 @@ public class MainActivity extends BaseActivity {
         return list;
     }
 
+    /*设置今天的Flag高亮*/
     private void setFlag(DateTime dateTime) {
         switch (dateTime.getDayOfWeek())
         {
@@ -260,24 +282,7 @@ public class MainActivity extends BaseActivity {
         }
     }
 
-    private int[] setDays(DateTime dateTime) {
-        int[] days=new int[7];
-        //计算出当前时刻本周一的值
-        DateTime thisMonday=dateTime.minusDays(dateTime.getDayOfWeek() - 1);
-        for (int i=0;i<=6;i++)
-        {
-            days[i]=thisMonday.plusDays(i).getDayOfMonth();
-        }
-        return days;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        initService();
-    }
-
-
+    /*根据用户名和DateTime获取任务*/
     private void getTasks(String username,DateTime dateTime) {
         AVQuery<TaskItem> query=AVObject.getQuery(TaskItem.class);
         query.whereEqualTo("userName", username);
@@ -302,34 +307,7 @@ public class MainActivity extends BaseActivity {
         });
     }
 
-
-    //禁用返回
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        // TODO Auto-generated method stub
-        // 连按两次返回键退出程序
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if ((System.currentTimeMillis() - exitTime) < 2000) {
-                finish();
-                System.exit(0);
-            }
-            exitTime = System.currentTimeMillis();
-            Toast.makeText(this, "再按一次退出程序！", Toast.LENGTH_SHORT).show();
-        }
-        return false;
-    }
-
-    private void initService() {
-        mUser=AVUser.getCurrentUser();
-        if (mUser!=null){
-            getTasks(mUser.getUsername(),dateTimeNow);
-        }else {
-        }
-    }
-
-    /**
-     * 绑定数据
-     */
+    /*绑定任务数据*/
     private void bindTaskItem(List<TaskItem> list) {
         if (list==null)
         {
@@ -337,27 +315,18 @@ public class MainActivity extends BaseActivity {
         }else {
             Log.d("test", "itemlist不为空");
             taskList=list;
+
+            listAdapter=new CommonAdapter<TaskItem>(this,list,R.layout.taskitem) {
+                @Override
+                public void convert(BaseViewHolder helper, TaskItem item) {
+                    helper.setText(R.id.task_title, item.getTaskTitle())
+                            .setText(R.id.task_content, item.getTaskContent())
+                            .setText(R.id.task_time,item.getTaskStartDateTime().toString("HH:mm"));
+                }
+
+            };
+            mListView.setAdapter(listAdapter);
+            listAdapter.notifyDataSetChanged();
         }
-
-
-
-        listAdapter=new CommonAdapter<TaskItem>(this,list,R.layout.taskitem) {
-            @Override
-            public void convert(BaseViewHolder helper, TaskItem item) {
-                helper.setText(R.id.task_title, item.getTaskTitle())
-                        .setText(R.id.task_content, item.getTaskContent())
-                        .setText(R.id.task_time,item.getTaskStartDateTime().toString("HH:mm"));
-            }
-
-        };
-        mListView.setAdapter(listAdapter);
-        listAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_activity_actions, menu);
-        return super.onCreateOptionsMenu(menu);
     }
 }
--- ld01/src/main/java/com/example/kk/ld01/fragments/DayFragment.java
+++ ld01/src/main/java/com/example/kk/ld01/fragments/DayFragment.java
@@ -1,24 +1,45 @@
 package com.example.kk.ld01.fragments;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.AVQuery;
+import com.avos.avoscloud.AVUser;
+import com.avos.avoscloud.FindCallback;
 import com.example.kk.ld01.R;
+import com.example.kk.ld01.models.TaskItem;
+import com.example.kk.ld01.utils.BaseViewHolder;
+import com.example.kk.ld01.utils.CommonAdapter;
 
 import org.joda.time.DateTime;
+
+import java.util.List;
 
 /**
  * Created by KK on 2016/1/29.
  */
+
 public class DayFragment extends Fragment {
     private ListView mListView;
+    private TextView mTextView;
+    private AVUser mUser;
 
     private DateTime dateTime;
+    private List<TaskItem> taskList;
+    private CommonAdapter<TaskItem> listAdapter;
+    private long exitTime = 0;
+    private DateTime dateTimeNow;
+    private List<DateTime> dateTimeThisWeek;
 
     public DayFragment(DateTime dateTime) {
         this.dateTime = dateTime;
@@ -27,7 +48,7 @@ public class DayFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fagment_day,null);
+        return inflater.inflate(R.layout.fragment_day,null);
     }
 
     @Override
@@ -37,11 +58,65 @@ public class DayFragment extends Fragment {
         initService();
     }
 
+    /*初始化服务请求*/
     private void initService() {
+        mUser= AVUser.getCurrentUser();
+        if (mUser!=null){
+            mTextView.setText(mUser.getUsername()+dateTime.getDayOfWeek());
+//            getTasks(mUser.getUsername(),dateTime);
+        }else {
+        }
+    }
 
+    /*根据用户名和DateTime获取任务*/
+    private void getTasks(String username,DateTime dateTime) {
+        AVQuery<TaskItem> query= AVObject.getQuery(TaskItem.class);
+        query.whereEqualTo("userName", username);
+        //TODO 测试通过DayOfYear来获取任务
+//        query.whereEqualTo("taskStartDateTime.getDayOfWeek()", dateTime.getDayOfWeek());
+        query.include("taskStartDateTime.getDayOfWeek()");
+        Log.d("test", "getTasksOf " + dateTime.getDayOfYear());
+        query.findInBackground(new FindCallback<TaskItem>() {
+            @Override
+            public void done(List<TaskItem> list, AVException e) {
+                if (e == null) {
+                    Log.d("test", "getTasks:" + list.size() + "条数据");
+                    for (TaskItem task : list) {
+                        Log.d("test", "日期"+task.getTaskStartDateTime().getDayOfYear());
+                    }
+                    bindTaskItem(list);
+                } else {
+                    Log.d("test", "错误：" + e.getMessage());
+                }
+            }
+        });
+    }
+
+    /*绑定任务数据*/
+    private void bindTaskItem(List<TaskItem> list) {
+        if (list==null)
+        {
+
+        }else {
+            Log.d("test", "itemlist不为空");
+            mTextView.setVisibility(View.GONE);
+            taskList=list;
+            listAdapter=new CommonAdapter<TaskItem>(getActivity(),list,R.layout.taskitem) {
+                @Override
+                public void convert(BaseViewHolder helper, TaskItem item) {
+                    helper.setText(R.id.task_title, item.getTaskTitle())
+                            .setText(R.id.task_content, item.getTaskContent())
+                            .setText(R.id.task_time, item.getTaskStartDateTime().toString("HH:mm"));
+                }
+
+            };
+            mListView.setAdapter(listAdapter);
+            listAdapter.notifyDataSetChanged();
+        }
     }
 
     private void initViews() {
         mListView= (ListView) getView().findViewById(R.id.listview_day);
+        mTextView= (TextView) getView().findViewById(R.id.textview_day);
     }
 }
